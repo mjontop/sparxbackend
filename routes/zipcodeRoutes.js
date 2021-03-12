8e59ddc54802435ecd1c8afce26b79a5e51650b5
Manikant Jha <mjontop0602@gmail.com>
2021-03-12 14:05:39 +0530

    zip code updated

--- routes/zipcodeRoutes.js
+++ routes/zipcodeRoutes.js
@@ -3,8 +3,18 @@ const Zipcode = require('../models/Zipcodemodel');
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-  const zipcode = await Zipcode.findOne({ where: { zip: req.params.id } }, { raw: true });
-  return res.send(zipcode);
+  try {
+    const zipcode = await Zipcode.findOne({ where: { zip: req.params.id }, raw:true });
+    if (!zipcode) return res.send({ error: 'Invalid Zipcode' });
+    for (var key in zipcode) {
+      if (!zipcode[key]) {
+        zipcode[key] = 'NA';
+      }
+    }
+    return res.send(zipcode);
+  } catch (ex) {
+    return res.send({ sucess: false, error: ex });
+  }
 });
 
 module.exports = router;
